b667f86525d8bebed3dedd4e2d46ad6b32517391
rbrightsingh-sys <rbrightsingh@gmail.com>
2026-08-14 16:28:58 +0530

    removing the api key

--- backend/main.py
+++ backend/main.py
@@ -208,10 +208,8 @@ def get_submission(job_id: str) -> Optional[dict]:
 
 # fix this hardcoded key and model in the code, and use the .env file instead
 
-GEMINI_API_KEY =
-# 'AQ.Ab8RN6IpNJwQEcxzE_k6cqTIEICtN_tFeAVuzbZL8F9dM-1dlg'
-# 'AQ.Ab8RN6IMfsQvZxBFU1pnPH1liEL8CWDsnyRvluak_9f4OqFPjw'
-# "AQ.Ab8RN6LYGim4FShwxBmoscT8U-cQ-fga28TE_cg0oRug88wUKQ"
+GEMINI_API_KEY =""
+
  
 # os.getenv("GEMINI_API_KEY")
 GEMINI_MODEL ="gemini-3.5-flash"
